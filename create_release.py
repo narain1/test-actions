71e8e0fb60eaf07771e07b122fb6de161d82c3dc
Narain <npattab1@asu.edu>
2024-10-08 13:12:49 -0700

    Update create_release.py

--- create_release.py
+++ create_release.py
@@ -1,19 +1,13 @@
-import argparse
 import subprocess
 import glob, os
 
-# Set up argument parsing
-parser = argparse.ArgumentParser(description="Create a GitHub release with a tag, title, and description.")
-parser.add_argument('--tag_name', required=True, help='The tag name for the release')
-parser.add_argument('--pr_title', required=True, help='The pull request title')
-parser.add_argument('--pr_description', required=True, help='The pull request description')
+tag_name = shutil.run("${{github.event.number }}", capture_output=True, text=True)
+pr_title = shutil.run("${{github.event.pull_request.title }}", capture_output=True, text=True)
+pr_description =shutil.run("${{github.event.pull_request.body}}", capture_output=True, text=True)
 
-# Parse the arguments
-args = parser.parse_args()
-
-tag_name = args.tag_name
-pr_title = args.pr_title
-pr_description = args.pr_description
+print("Tag name", tag_name)
+print("pr title", pr_title)
+print("pr description", pr_description)
 
 # Convert single newlines to newline with two spaces for Markdown formatting
 escaped_pr_description = pr_description.replace('\n', '  \n')
